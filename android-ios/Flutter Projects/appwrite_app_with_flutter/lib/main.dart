bb207ac54f96b64b198b4cf91812fa0018f782e1
VinuBalagopalAP <vinubalagopalap@gmail.com>
2021-10-30 22:04:20 +0530

    Flutter - Appwrite App

---
+++ android-ios/Flutter Projects/appwrite_app_with_flutter/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:appwrite_app_with_flutter/playground.dart';
+import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
+
+void main() {
+  Client client = Client();
+  Account account = Account(client);
+  Storage storage = Storage(client);
+  Database database = Database(client);
+
+  client
+          .setEndpoint(
+              'https://localhost/v1') // Make sure your endpoint is accessible from your emulator, use IP if needed
+          .setProject('60793ca4ce59e') // Your project ID
+          .setSelfSigned() // Do not use this in production
+      // .addHeader('Origin', 'http://localhost')
+      ;
+
+  runApp(MaterialApp(
+    home: Playground(
+      client: client,
+      account: account,
+      storage: storage,
+      database: database,
+    ),
+  ));
+}
