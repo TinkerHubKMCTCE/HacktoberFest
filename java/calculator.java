ed5d21ab0137b976c85d609afc7154b61f262450
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-10-31 06:36:09 -0700

    first commit

---
+++ java/calculator.java
@@ -0,0 +1,15 @@
+public class Minicalculator {
+    public static void main(String[]args){
+        double dub =add(4,2);
+        System.out.println(dub);
+        
+		double dub2 = mul(4,2);
+		System.out.println(dub2);
+		
+		double dub3 = sub(4,2);
+		System.out.println(dub3);
+		
+		double dub4 = mod(4,2);
+		System.out.println(dub4);
+    }
+   
