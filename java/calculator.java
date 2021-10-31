1c0ae8c646d6c23f5e13dde4e9500dcdad7f9b39
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-10-31 06:45:04 -0700

    java calculator

--- java/calculator.java
+++ java/calculator.java
@@ -12,4 +12,30 @@ public class Minicalculator {
 		double dub4 = mod(4,2);
 		System.out.println(dub4);
     }
+	 
+    public static double add(int x,int y) {
+		int c = x+y;
+		double d = (double)c;
+		switch(c) {
+		case 0:
+			return 0;
+		default:
+			return d;
+		}
+	}
+    public static double mul(int x,int y) {
+		int a = x * y;
+		double d = (double) a;
+		
+	   switch(a){
+	    case 0:
+	        return 0;
+	    default:
+	        return d;
+	    }
+	} 
+   	
+	
+
+		
    
