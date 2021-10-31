a1a70db8255cc03f4ae8adc86c1827addd570111
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-10-31 07:50:13 -0700

    Java project-Mini calculator

--- java/calculator.java
+++ java/calculator.java
@@ -1,18 +1,18 @@
 public class Minicalculator {
     public static void main(String[]args){
-                double dub =add(4,2);
+                double dub =add(5,2);
                 System.out.println(dub);
         
-		double dub2 = mul(4,2);
+		double dub2 = mul(5,2);
 		System.out.println(dub2);
 		
-		double dub3 = sub(4,2);
+		double dub3 = sub(5,2);
 		System.out.println(dub3);
 		
-		double dub4 = mod(4,2);
+		double dub4 = mod(5,2);
 		System.out.println(dub4);
 	        
-	        double dub5 = div(4,2);
+	        double dub5 = div(5,2);
 		System.out.println(dub5);
     }
 	 
